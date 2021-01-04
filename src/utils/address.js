7f7dd29352be679ccc0814dac55c2ad12d8cc740
chenjian <jian.chen@simesoft.cn>
2021-01-04 09:52:44 +0800

    生产

--- src/utils/address.js
+++ src/utils/address.js
@@ -1,7 +1,7 @@
 const baseImgUrl = ()=>{
     //http://192.168.0.150:3000/
-    //http://192.168.0.253:9301/ 测试地址
-    var imgUrl = 'http://221.226.240.134:10083/'
+    //http://221.226.240.134:10083 测试地址 http://manage.jabp-tiger.com:9000 生产
+    var imgUrl = 'http://manage.jabp-tiger.com:9000'
     return imgUrl
 }
 
